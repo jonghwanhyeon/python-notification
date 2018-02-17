9e7d98b5312269f4f3623c9c6859498c323c44b0
Jonghwan Hyeon <hyeon0145@gmail.com>
2018-02-18 00:29:56 +0900

    Add Pushover

---
+++ notification/__init__.py
@@ -0,0 +1,1 @@
+from .providers.pushover import Pushover
---
+++ notification/providers/pushover.py
@@ -0,0 +1,31 @@
+import requests
+
+from . import Provider
+from ..exceptions import NotificationException
+
+pushover_url = 'https://api.pushover.net/1/messages.json'
+allowed_parameters = { 'attachment', 'device', 'title', 'url', 'url_title', 'priority', 'sound', 'timestamp' }
+
+class Pushover(Provider):
+    def __init__(self, user, token):
+        self.user = user
+        self.token = token
+
+    def notify(self, message, **kwargs):
+        for key in kwargs:
+            if key not in allowed_parameters:
+                raise ValueError('Invalid parameter: {key}'.format(key=key))
+
+        parameters = dict(kwargs, user=self.user, token=self.token, message=message)
+
+        files = None
+        if 'attachment' in parameters:
+            files = { 'attachment': ('attachment.jpg', parameters['attachment'], 'image/jpeg') }
+
+        respoonse = requests.post(pushover_url, data=parameters, files=files)
+        respoonse_as_json = respoonse.json()
+
+        if respoonse.status_code != 200:
+            raise NotificationException(respoonse_as_json['errors'][0])
+
+        return respoonse_as_json['request']
